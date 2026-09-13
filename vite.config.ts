054a442ab1d4e41ecdbf21bf56e714dda3ed04c6
rezowan-kabir <rezyounkabir@gmail.com>
2026-09-13 20:01:35 +0600

    add base

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
-// Vite configuration with Tailwind CSS v4 support
 export default defineConfig({
-  plugins: [react(),tailwindcss()],});
+  base: './', 
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+});
